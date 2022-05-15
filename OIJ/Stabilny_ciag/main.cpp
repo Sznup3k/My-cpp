18b5e18b21e946478e9a95cd05297445e16a86cc
Stanisław Pęszkal <105589481+Sznup3k@users.noreply.github.com>
2022-05-15 21:27:21 +0200

    Update main.cpp

--- OIJ/Stabilny_ciag/main.cpp
+++ OIJ/Stabilny_ciag/main.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+
+
+using namespace std;
+
+
+
+int main() 
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    
+
+    return 0;
+}
 
